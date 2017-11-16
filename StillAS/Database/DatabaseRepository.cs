a78cc97bdf47390eba80fbeb9db15ef0bc9f0587
SpookyBooqie <33721140+SpookyBooqie@users.noreply.github.com>
2017-11-16 13:49:50 +0100

    Database Repository Class Skabt

--- StillAS/Database/DatabaseRepository.cs
+++ StillAS/Database/DatabaseRepository.cs
@@ -6,11 +6,8 @@ using System.Threading.Tasks;
 
 namespace Database
 {
-    class Program
+    class DatabaseRepository
     {
-        static void Main(string[] args)
-        {
 
-        }
     }
 }
