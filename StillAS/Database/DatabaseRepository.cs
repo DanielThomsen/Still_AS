0bb1ceacb51cf2685535010ab1d356b820539713
SpookyBooqie <33721140+SpookyBooqie@users.noreply.github.com>
2017-11-17 09:18:55 +0100

    CreateModelName Metode

--- StillAS/Database/DatabaseRepository.cs
+++ StillAS/Database/DatabaseRepository.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace Database
 {
     class DatabaseRepository
     {
-
+        public void CreateModelName(string ModelName)
+        {
+            var ModelN = new ModelNavn { Modelnavn1 = ModelName };
+            var meContext = new stillasEntities();
+            meContext.ModelNavns.Add(ModelN);
+            meContext.SaveChanges();
+        }
     }
 }
