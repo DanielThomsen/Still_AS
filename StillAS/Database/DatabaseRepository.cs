ceae97a54b09bad1da4468de5c9593fbee2264d1
SpookyBooqie <33721140+SpookyBooqie@users.noreply.github.com>
2017-11-20 11:49:46 +0100

    Tilføj Maskine functional

--- StillAS/Database/DatabaseRepository.cs
+++ StillAS/Database/DatabaseRepository.cs
@@ -21,7 +21,7 @@ namespace Database
             string AggregatNumber, string BatteryType, string BatteryNumber, string ChargerType, string ChargerNumber,
             string Controller, decimal Weight, decimal Height, decimal Length, decimal Width)
         {
-            var Machine = new Maskine { DemoNummer = DemoNumber, ModelNavn = ModelName, Type = ModelNumber, Fabrikant = Brand, Chassisnummer = CNumber,
+            var Machine = new Maskine { DemoNummer = DemoNumber, ModelName = ModelName, Type = ModelNumber, Fabrikant = Brand, Chassisnummer = CNumber,
                 MastType = MastType, MastByggeHøjde = MastBuildingHeight, MastLøfteHøjde = MastLiftHeight, MastFriLøft = MastFreeLift,
                 Aggregat = AggregatType, AggregarNummer = AggregatNumber, BatteriType = BatteryType, BatteriNummer = BatteryNumber,
                 LaderType = ChargerType, LaderNummer = ChargerNumber, Betjening = Controller, Weight = Weight, Height = Height,
