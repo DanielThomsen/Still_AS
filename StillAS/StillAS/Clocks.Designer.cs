c22a94702ea32236d067b25a333b650e5521443a
SpookyBooqie <33721140+SpookyBooqie@users.noreply.github.com>
2017-11-27 10:02:00 +0100

    Clocks created

---
+++ StillAS/StillAS/Clocks.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace StillAS
+{
+    partial class Clocks
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCph = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblCph
+            // 
+            this.lblCph.AutoSize = true;
+            this.lblCph.Location = new System.Drawing.Point(217, 30);
+            this.lblCph.Name = "lblCph";
+            this.lblCph.Size = new System.Drawing.Size(89, 17);
+            this.lblCph.TabIndex = 0;
+            this.lblCph.Text = "Copenhagen";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(196, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 52);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "00:00";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(226, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "New York";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(196, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 52);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "00:00";
+            // 
+            // Clocks
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 253);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblCph);
+            this.Name = "Clocks";
+            this.Text = "Clocks";
+            this.Load += new System.EventHandler(this.Clocks_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCph;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
