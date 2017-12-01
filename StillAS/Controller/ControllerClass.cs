2c2ae61aac55d0500eec83b2b9088a16a0ed2d17
SpookyBooqie <33721140+SpookyBooqie@users.noreply.github.com>
2017-12-01 11:30:05 +0100

    Clock methods relocated to Model

--- StillAS/Controller/ControllerClass.cs
+++ StillAS/Controller/ControllerClass.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Database;
+using Model;
 using System.Data;
 
 namespace Controller
 {
     public class ControllerClass
     {
+        ModelClass MC = new ModelClass();
         DatabaseRepository DBR = new DatabaseRepository();
         public static List<string> DemoMachines = new List<string>();
         //[Krognos start]
@@ -56,84 +58,12 @@ namespace Controller
             // SKal til Model
         public string TimeNow()
         {
-            int Hour = DateTime.Now.Hour;
-            int Minute = DateTime.Now.Minute;
-            int Second = DateTime.Now.Second;
-            
-            string time = "";
-
-            if (Hour < 10)
-            {
-                time += "0" + Hour;
-            }
-            else
-            {
-                time += Hour;
-            }
-
-            time += ":";
-
-            if (Minute < 10)
-            {
-                time += "0" + Minute;
-            }
-            else
-            {
-                time += Minute;
-            }
-
-            time += ":";
-
-            if (Second < 10)
-            {
-                time += "0" + Second;
-            }
-            else
-            {
-                time += Second;
-            }
-            return time;
+            return MC.ClockNow();
         }
         //Skal til model
         public string TimeNY()
         {
-            DateTime DT = DateTime.Now.AddHours(-6);
-            int Hour = DT.Hour;
-            int Minute = DateTime.Now.Minute;
-            int Second = DateTime.Now.Second;
-            string time = "";
-
-            if (Hour < 10)
-            {
-                time += "0" + Hour;
-            }
-            else
-            {
-                time += Hour;
-            }
-
-            time += ":";
-
-            if (Minute < 10)
-            {
-                time += "0" + Minute;
-            }
-            else
-            {
-                time += Minute;
-            }
-
-            time += ":";
-
-            if (Second < 10)
-            {
-                time += "0" + Second;
-            }
-            else
-            {
-                time += Second;
-            }
-            return time;
+            return MC.ClockNY();
         }
         //[Krognos slut]
 
